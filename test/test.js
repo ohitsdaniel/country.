ac821e00eb3205444d54f0f7fb637ef44ddcd68d
Daniel Peter <daniel.peter@me.com>
2017-02-10 13:30:57 +0100

    invalid language exception + tests

--- test/test.js
+++ test/test.js
@@ -6,12 +6,15 @@ describe('lib.names', function() {
 		it('ISO1->Name', function() {
 			assert.include(lib.names('276'), 'Germany');
 		});
+		
 		it('ISO2->Name', function() {
 			assert.include(lib.names('DE'), 'Germany');
 		});
+
 		it('ISO3->Name', function() {
 			assert.include(lib.names('DEU'), 'Germany');
 		});
+
 		it('Name->Name', function() {
 			assert.include(lib.names('Germany'), 'Germany');
 		});
@@ -20,12 +23,15 @@ describe('lib.names', function() {
 		it('ISO1->Name', function() {
 			assert.include(lib.names('826'), 'United Kingdom');
 		});
+
 		it('ISO2->Name', function() {
 			assert.include(lib.names('GB'), 'UK');
 		});
+
 		it('ISO3->Name', function() {
 			assert.include(lib.names('GBR'), 'Great Britain');
 		});
+
 		it('Name->Name', function() {
 			assert.include(lib.names('United Kingdom'), 'UK');
 		});
@@ -35,18 +41,33 @@ describe('lib.names', function() {
 		it('ISO1->Name', function() {
 			assert.include(lib.names('840'), 'United States');
 		});
+
 		it('ISO2->Name', function() {
 			assert.include(lib.names('US'), 'USA');
 		});
+
 		it('ISO3->Name', function() {
 			assert.include(lib.names('USA'), 'US');
 		});
+
 		it('Name->Name', function() {
 			assert.include(lib.names('United States'), 'USA');
 		});
 	});
 
 	describe('#Exceptional behaviour', function() {
+		it('German name', function() {
+			assert.include(lib.names('Deutschland', 'de'), 'Deutschland');
+		});
+
+		it('Unsupported language', function() {
+			try {
+				lib.names('Deutschland', 'asdf');
+			} catch (err) {
+				assert(/INVALIDLANGUAGE/.test(err));
+			}
+		});
+
 		it('trim', function() {
 			assert.include(lib.names(' 276'), 'Germany');
 			assert.include(lib.names('276 '), 'Germany');
@@ -54,7 +75,7 @@ describe('lib.names', function() {
 		});
 
 		it('Exception Handling', function() {
-			assert.throws(lib.names, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY/)
+			assert.throws(lib.names, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY|INVALIDLANGUAGE/)
 		});
 	});
 })
@@ -64,9 +85,11 @@ describe('lib.iso2', function() {
 		it('ISO1->ISO2', function() {
 			assert.equal(lib.iso2('276'), 'DE');
 		});
+
 		it('ISO2->ISO2', function() {
 			assert.equal(lib.iso2('DE'), 'DE');
 		});
+
 		it('ISO3->ISO2', function() {
 			assert.equal(lib.iso2('DEU'), 'DE');
 		});
@@ -76,13 +99,26 @@ describe('lib.iso2', function() {
 		});
 	});
 	describe('#Exceptional behaviour', function() {
+		it('German name', function() {
+			assert.equal(lib.iso2('Deutschland', 'de'), 'DE');
+		});
+
+		it('Unsupported language', function() {
+			try {
+				lib.iso2('Deutschland', 'asdf');
+			} catch (err) {
+				assert(/INVALIDLANGUAGE/.test(err));
+			}
+		});
+
 		it('trim needed', function() {
 			assert.equal(lib.iso2(' 276'), 'DE');
 			assert.equal(lib.iso2('276 '), 'DE');
 			assert.equal(lib.iso2(' 276 '), 'DE');
 		});
+
 		it('Exception Handling', function() {
-			assert.throws(lib.iso2, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY/);
+			assert.throws(lib.iso2, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY|INVALIDLANGUAGE/);
 		});
 	});
 });
@@ -92,9 +128,11 @@ describe('lib.iso3', function() {
 		it('ISO1->ISO3', function() {
 			assert.equal(lib.iso3('276'), 'DEU');
 		});
+
 		it('ISO2->ISO3', function() {
 			assert.equal(lib.iso3('DE'), 'DEU');
 		});
+
 		it('ISO3->ISO3', function() {
 			assert.equal(lib.iso3('DEU'), 'DEU');
 		});
@@ -104,13 +142,26 @@ describe('lib.iso3', function() {
 		});
 	});
 	describe('#Exceptional behaviour', function() {
+		it('German name', function() {
+			assert.equal(lib.iso3('Deutschland', 'de'), 'DEU');
+		});
+
+		it('Unsupported language', function() {
+			try {
+				assert.equal(lib.iso3('Deutschland', 'asdf'), 'DEU');
+			} catch (err) {
+				assert(/INVALIDLANGUAGE/.test(err));
+			}
+		});
+
 		it('trim needed', function() {
 			assert.equal(lib.iso3(' 276'), 'DEU');
 			assert.equal(lib.iso3('276 '), 'DEU');
 			assert.equal(lib.iso3(' 276 '), 'DEU');
 		});
+
 		it('Exception Handling', function() {
-			assert.throws(lib.iso3, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY/);
+			assert.throws(lib.iso3, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY|INVALIDLANGUAGE/);
 		});
 	});
 });
@@ -120,9 +171,11 @@ describe('lib.country', function() {
 		it('ISO1->Country', function() {
 			assert.equal(lib.country('276').ISO1, '276');
 		});
+
 		it('ISO2->Country', function() {
 			assert.equal(lib.country('DE').ISO2, 'DE');
 		});
+
 		it('ISO3->Country', function() {
 			assert.equal(lib.country('DEU').ISO3, 'DEU');
 		});
@@ -132,13 +185,26 @@ describe('lib.country', function() {
 		});
 	});
 	describe('#Exceptional behaviour', function() {
+		it('German name', function() {
+			assert.equal(lib.country('Deutschland', 'de').ISO3, 'DEU');
+		});
+
+		it('Unsupported language', function() {
+			try {
+				lib.country('Deutschland', 'asdf');
+			} catch (err) {
+				assert(/INVALIDLANGUAGE/.test(err));
+			}
+		});
+
 		it('trim needed', function() {
 			assert.equal(lib.country(' 276').ISO1, '276');
 			assert.equal(lib.country('276 ').ISO1, '276');
 			assert.equal(lib.country(' 276 ').ISO1, '276');
 		});
+
 		it('Exception Handling', function() {
-			assert.throws(lib.country, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY/);
+			assert.throws(lib.country, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY|INVALIDLANGUAGE/);
 		});
 	});
 });
@@ -153,6 +219,7 @@ describe('lib.languages', function() {
 				"name": "German"
 			});
 		});
+
 		it('ISO2->languages', function() {
 			assert.include(lib.languages('DE'), {
 				"alpha2": "de",
@@ -161,6 +228,7 @@ describe('lib.languages', function() {
 				"name": "German"
 			});
 		});
+
 		it('ISO3->languages', function() {
 			assert.include(lib.languages('DEU'), {
 				"alpha2": "de",
@@ -180,6 +248,23 @@ describe('lib.languages', function() {
 		});
 	});
 	describe('#Exceptional behaviour', function() {
+		it('German name', function() {
+			assert.include(lib.languages('Deutschland', 'de'), {
+				"alpha2": "de",
+				"alpha3": "deu",
+				"bibliographic": "ger",
+				"name": "German"
+			});
+		});
+
+		it('Unsupported language', function() {
+			try {
+				lib.languages('Deutschland', 'asdf');
+			} catch (err) {
+				assert(/INVALIDLANGUAGE/.test(err));
+			}
+		});
+
 		it('trim needed', function() {
 			assert.include(lib.languages(' 276'), {
 				"alpha2": "de",
@@ -200,8 +285,9 @@ describe('lib.languages', function() {
 				"name": "German"
 			});
 		});
+
 		it('Exception Handling', function() {
-			assert.throws(lib.languages, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY/);
+			assert.throws(lib.languages, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY|INVALIDLANGUAGE/);
 		});
 	});
 });
@@ -217,6 +303,7 @@ describe('lib.currencies', function() {
 				"symbol": "€"
 			});
 		});
+
 		it('ISO2->currencies', function() {
 			assert.include(lib.currencies('DE'), {
 				"currency": "Euro",
@@ -226,6 +313,7 @@ describe('lib.currencies', function() {
 				"symbol": "€"
 			});
 		});
+
 		it('ISO3->currencies', function() {
 			assert.include(lib.currencies('DEU'), {
 				"currency": "Euro",
@@ -247,6 +335,24 @@ describe('lib.currencies', function() {
 		});
 	});
 	describe('#Exceptional behaviour', function() {
+		it('German name', function() {
+			assert.include(lib.currencies('Deutschland', 'de'), {
+				"currency": "Euro",
+				"alphaCode": "EUR",
+				"numCode": "978",
+				"minorUnit": "2",
+				"symbol": "€"
+			});
+		});
+
+		it('Unsupported language', function() {
+			try {
+				lib.currencies('Deutschland', 'asdf');
+			} catch (err) {
+				assert(/INVALIDLANGUAGE/.test(err));
+			}
+		});
+
 		it('trim needed', function() {
 			assert.include(lib.currencies(' 276'), {
 				"currency": "Euro",
@@ -270,8 +376,9 @@ describe('lib.currencies', function() {
 				"symbol": "€"
 			});
 		});
+
 		it('Exception Handling', function() {
-			assert.throws(lib.languages, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY/);
+			assert.throws(lib.currencies, /INVALIDCODE|INVALIDFORMAT|INVALIDCOUNTRY|INVALIDLANGUAGE/);
 		});
 	});
 });
